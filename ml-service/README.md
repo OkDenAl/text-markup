e0a558a9be4afe88439ee5f05e7dc11a97388c65
Николай Степанов <nikolay.stepanov@vk.team>
2024-03-16 17:23:26 +0300

    baseline and simple ml service

---
+++ ml-service/README.md
@@ -0,0 +1,3 @@
+Данные: https://natasha.github.io/nerus/
+
+Модель для бейзлайна: https://huggingface.co/viktoroo/sberbank-rubert-base-collection3
---
+++ ml-service/service.py
@@ -0,0 +1,40 @@
+from fastapi import FastAPI
+from transformers import AutoTokenizer, AutoModelForTokenClassification
+import torch
+import uvicorn
+from pydantic import BaseModel
+
+
+class Item(BaseModel):
+    text: str
+
+
+app = FastAPI()
+
+tokenizer = AutoTokenizer.from_pretrained("viktoroo/sberbank-rubert-base-collection3")
+model = AutoModelForTokenClassification.from_pretrained("viktoroo/sberbank-rubert-base-collection3")
+
+
+@app.post("/get_prediction")
+async def get_prediction(item: Item):
+    try:
+        text = item.text
+        inputs = tokenizer(text, return_tensors="pt")
+
+        with torch.no_grad():
+            outputs = model(**inputs)
+
+        predicted_labels = torch.argmax(outputs.logits, dim=2).squeeze().tolist()
+
+        token_list = tokenizer.convert_ids_to_tokens(inputs["input_ids"].squeeze().tolist())
+
+        token_labels = [model.config.id2label[label_id] for label_id in predicted_labels]
+
+        print(token_list, token_labels)
+
+        return {"tokens": token_list, "labels": token_labels}
+    except Exception as e:
+        print(e)
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="localhost", port=8080)
