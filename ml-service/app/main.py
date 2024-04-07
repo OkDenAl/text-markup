8a61bc69ba6fdce4f4f0b1f55ac8e75597729a3d
Николай Степанов <nikolay.stepanov@vk.team>
2024-04-07 20:06:02 +0300

    add transform of tokens and label for human-read form

--- ml-service/app/main.py
+++ ml-service/app/main.py
@@ -13,7 +13,38 @@ app = FastAPI()
 tokenizer = AutoTokenizer.from_pretrained("viktoroo/sberbank-rubert-base-collection3")
 model = AutoModelForTokenClassification.from_pretrained("viktoroo/sberbank-rubert-base-collection3")
 
-@app.get("/api/v1/prediction")
+
+def transform_tag(tag):
+    return tag.replace(" ##", "").replace(" . ", ".").replace("и ", "й ").strip().title()
+
+
+def transform_model_output(token_list, token_labels):
+    tag = ""
+    tag_label = ""
+    tags = []
+    tag_labels = []
+
+    for token, label in zip(token_list, token_labels):
+        if label == "O":
+            if tag != "":
+                tags.append(transform_tag(tag))
+                tag_labels.append(tag_label)
+                tag = ""
+                tag_label = ""
+            continue
+        if label.startswith("B"):
+            if tag != "":
+                tags.append(transform_tag(tag))
+                tag_labels.append(tag_label)
+                tag = ""
+            tag += (" " + token)
+            tag_label = label[-3:]
+        if label.startswith("I"):
+            tag += (" " + token)
+
+    return tags, tag_labels
+
+@app.get("api/v1/prediction")
 async def get_prediction(item: Item):
     try:
         text = item.text
@@ -27,6 +58,8 @@ async def get_prediction(item: Item):
         token_list = tokenizer.convert_ids_to_tokens(inputs["input_ids"].squeeze().tolist())
 
         token_labels = [model.config.id2label[label_id] for label_id in predicted_labels]
+
+        token_list, token_labels = transform_model_output(token_list, token_labels)
 
         print(token_list, token_labels)
 
