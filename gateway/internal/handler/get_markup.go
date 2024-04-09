72050f3c2850072f17686d59b65bc707b7c44568
d.okutin <d.okutin@tinkoff.ru>
2024-04-09 21:50:04 +0300

    add new handler

--- gateway/internal/handler/get_markup.go
+++ gateway/internal/handler/get_markup.go
@@ -4,18 +4,18 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/pkg/errors"
 
 	"github.com/OkDenAl/text-markup-gateway/internal/handler/model"
 	"github.com/OkDenAl/text-markup-gateway/internal/handler/responses"
-	"github.com/OkDenAl/text-markup-gateway/pkg/logger"
 )
 
 // @BasePath /api/v1
 
 // getMarkup godoc
-// @Summary get markup from text (string)
+// @Summary get markup from text
 // @Schemes
-// @Description get markup from text (string)
+// @Description get markup from text
 // @Tags Markup
 // @Produce json
 // @Param   text body model.TextMarkupRequest  true  "Text JSON"
@@ -24,21 +24,20 @@ import (
 // @Router /markup [post]
 func getMarkup(markup iMLMarkup) gin.HandlerFunc {
 	return func(c *gin.Context) {
-		log := logger.New()
 		req := model.TextMarkupRequest{}
 		if err := c.BindJSON(&req); err != nil {
-			log.Error().Stack().Err(err).Msg("failed to get request data")
-			c.JSON(http.StatusBadRequest, responses.Error(err))
+			c.JSON(http.StatusBadRequest, responses.Error(errors.Wrap(err, "failed to get request data")))
+			_ = c.AbortWithError(http.StatusBadRequest, err)
 			return
 		}
 
-		link, err := markup.GetEntitiesFromText(c, req.Text)
+		entities, err := markup.GetEntitiesFromText(c, req.Text)
 		if err != nil {
-			log.Error().Stack().Err(err).Msg("failed to get entities from text")
 			c.JSON(http.StatusInternalServerError, responses.Error(err))
+			_ = c.AbortWithError(http.StatusInternalServerError, err)
 			return
 		}
 
-		c.JSON(http.StatusOK, link)
+		c.JSON(http.StatusOK, entities)
 	}
 }
